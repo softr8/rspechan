38192b66b2f6ef83cdb0ca35ddeb5e40dd033c59
Edwin Cruz <edwin.cruz@crowdint.com>
2012-03-23 17:58:53 -0600

    Fixing issue visiting products controller show action

--- spec/views/projects/show.html.erb_spec.rb
+++ spec/views/projects/show.html.erb_spec.rb
@@ -2,26 +2,14 @@ require 'spec_helper'
 
 describe "projects/show" do
   before(:each) do
-    @project = assign(:project, stub_model(Project,
-      id: 1,
-      name: "Name",
-      repo: "Repo",
-      description: "MyText",
-      ruby_version: "Ruby Version"
-    ))
     OrganizationHelper.default_id = 1
-
+    @project = Project.find_by_name('blog')
   end
 
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Name/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Repo/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/MyText/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Ruby Version/)
+    rendered.should match(/blog/)
+    rendered.should match(/ruby-1.9.3-rc1/)
   end
 end
